337fecf414b6fc3b967e79dbc0a1877217306aa0
hayndehayn <vualkalox1@gmail.com>
2024-10-25 18:05:12 +0200

    41

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,14 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import UncontrolledForm from './components/UncontrolledForm'
+
 import ControlledForm from './components/ControlledForm'
 import Fetcher from './components/Fetcher'
+import MyComponent from './components/Axios'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <UncontrolledForm />
     <ControlledForm />  
     <Fetcher />
+    <MyComponent />
   </StrictMode>,
 )
