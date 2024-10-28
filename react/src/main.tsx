9038ce1cdea993a133d10edc51bbda7497ea9e97
hayndehayn <vualkalox1@gmail.com>
2024-10-28 17:27:45 +0100

    UncontrolledForm

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -4,11 +4,13 @@ import { createRoot } from 'react-dom/client'
 import ControlledForm from './components/ControlledForm'
 import Fetcher from './components/Fetcher'
 import UserComponent from './components/UserComponent'
+import UncontrolledForm from './components/UncontrolledForm'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ControlledForm />  
+    <ControlledForm />
+    <UncontrolledForm />
     <Fetcher />
     <UserComponent />
   </StrictMode>,
